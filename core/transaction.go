df528939df71745e346f16971a5e1199ba5d1405
andrew <andrewayiko40@gmail.com>
2025-04-08 16:28:59 +0300

    Refactor Transaction methods to use pointer receiver for Sign and Verify; ensure proper signature handling

--- core/transaction.go
+++ core/transaction.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"fmt"
+
 	"github.com/Ayikoandrew/atendele/crypto"
 	"github.com/Ayikoandrew/atendele/types"
 )
@@ -15,13 +17,27 @@ type Transaction struct {
 	signature crypto.Signature
 }
 
-func (tx Transaction) Sign(priv crypto.PrivateKey) error {
-	priv.Sign(tx.Data)
+func (tx *Transaction) Sign(priv crypto.PrivateKey) error {
+	data := fmt.Sprintf("%+v %+v %+v", tx.To, tx.Value, tx.Data)
+	sig, err := priv.Sign([]byte(data))
+	if err != nil {
+		return err
+	}
+
+	tx.signature = sig
+	tx.publicKey = priv.PublicKey()
 	return nil
 }
 
-func (tx Transaction) Verify() error {
-	tx.publicKey.Verify(tx.Data, tx.signature)
+func (tx *Transaction) Verify() error {
+	data := fmt.Sprintf("%+v %+v %+v", tx.To, tx.Value, tx.Data)
+	var zeroSig = &crypto.Signature{}
+	if tx.signature == *zeroSig {
+		return fmt.Errorf("transaction does not have a signature")
+	}
+	if !tx.publicKey.Verify([]byte(data), tx.signature) {
+		return fmt.Errorf("signature verification failed")
+	}
 
 	return nil
 }
